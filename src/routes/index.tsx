bc092b1b1d0c9c5b4c7c1d8073050569e8463008
Jake Bogan <jakebogan01@gmail.com>
2023-03-23 14:32:52 -0400

    created test layout and index to test states

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,7 @@
-import { component$ } from '@builder.io/qwik';
+import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-
-
   return (
     <div>
       <h2 class="font-geo font-bold m-0 text-[2em]">Okie Dokie</h2>
--- src/routes/test/index@test.tsx
+++ src/routes/test/index@test.tsx
@@ -1,13 +1,31 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useStore } from "@builder.io/qwik";
 
 export default component$(() => {
+  const name = useSignal('yoshi') //normal data type variables
+
+  const person = useStore({ name: "peach", age: 30 }) //stores object/arrays with multiple properties
+
+  const blogs = useStore([
+    {id: 1, title: 'My first blog'},
+    {id: 2, title: 'My second blog'},
+    {id: 3, title: 'My third blog'},
+  ])
 
   return (
     <article class="bg-white p-5 rounded-lg drop-shadow-md m-2.5">
       <h2 class="font-geo font-bold m-0 text-[1.8em]">Test Layout</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci at excepturi facilis neque quidem reiciendis tenetur totam vitae voluptatem voluptates?</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci at excepturi facilis neque quidem reiciendis tenetur totam vitae voluptatem voluptates?</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci at excepturi facilis neque quidem reiciendis tenetur totam vitae voluptatem voluptates?</p>
+
+      <p>Hello, {name.value}</p>
+      <p>Hello, {person.name} - {person.age}</p>
+
+      <button onClick$={() => name.value = "Mario"}>Click me</button> {/*click event listener*/}
+      <button onClick$={() => person.name = "Bowser"}>Click me again</button>
+
+      {blogs.map(blog => (
+        <div key={blog.id}>{blog.title}</div>
+      ))}
+
+      <button onClick$={() => blogs.pop()}>Remove blog</button>
     </article>
   )
 });
